6fa1aed5c09010587a5d7f54b9f41be12066abfa
shrutikabhombe26 <153824777+shrutikabhombe26@users.noreply.github.com>
2026-05-21 11:15:21 +0100

    Api mapping name change

--- booking-service/src/main/java/com/microservices/booking_service/controller/BookingController.java
+++ booking-service/src/main/java/com/microservices/booking_service/controller/BookingController.java
@@ -1,8 +1,10 @@
 package com.microservices.booking_service.controller;
 
 import java.util.Optional;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,6 @@ import com.microservices.booking_service.entity.Booking;
 import com.microservices.booking_service.services.BookingService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 
 @RestController
@@ -26,22 +22,12 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping("/create")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
-    }
-    
-    @GetMapping("/getBooking/{id}")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
-    
-    public Booking createBooking(Booking booking) {
+    public Booking createBooking(@RequestBody Booking booking) {
         return bookingService.createBooking(booking);
     }
 
-    public Booking getBookingById(Long id) {
+    @GetMapping("/getBooking/{id}")
+    public Booking getBookingById(@PathVariable Long id) {
         Optional<Booking> bookingOptional = Optional.ofNullable(bookingService.getBookingById(id));
         if(bookingOptional.isPresent()) {
             return bookingOptional.get();
